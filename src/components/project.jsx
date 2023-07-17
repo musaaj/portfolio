993cfb9f375071be24b8dbf4a96b46df42359c93
Musa Ibrahim <musaaj@gmail.com>
2023-07-17 14:04:33 +0100

    correct tabs scrolling

--- src/components/project.jsx
+++ src/components/project.jsx
@@ -5,12 +5,12 @@ export default function Project({title, link, picture, stack, about})
 {
   return (
     <div className="w-full p-4 border-b-[0.3px] border-solid border-slat-400">
-      <div className="flex items-center">
+      <div className="flex items-center py-2">
         <h1 className="text-base font-bold">{title}</h1>
-        <span className="flex justify-end flex-grow"><a className="hover:bg-[--btn-secondary-hover] p-2"><Github /></a></span>
+        <span className="flex justify-end flex-grow"><a href={link} className="hover:bg-[--btn-secondary-hover] p-2 cursor-pointer"><Github /></a></span>
       </div>
       <img src={picture} className="rounded-xl" />
-      <p className="text-base text-ellipsis">{about}</p>
+      <p className="py-2 text-base text-ellipsis">{about}</p>
       <div className="flex">
         <span className="font-bold">Stack: </span>
         <span className="ml-2 font-thin text-blue-600">{stack}</span>
--- src/components/tabs.jsx
+++ src/components/tabs.jsx
@@ -9,7 +9,7 @@ export default function Tabs(props)
   const onTab = e=>setSelectedTab(e.target.getAttribute("id"));
   return (
     <>
-      <div className="flex items-center p-0 my-2 overflow-scroll">
+      <div className="flex items-center p-0 my-2 overflow-scroll scroller-none">
         {tabs.map((tab,idx)=><button id={idx} className={(selectedTab == idx? "border-b-4" : null)+" p-3 font-bold border-blue-600 border-solid grow-2 btn-secondary min-w-max"} onClick={onTab}>{tab.title}</button>)}
       </div>
       <div className="w-full p-0">
